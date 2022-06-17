101f73555442fe2a3a048b15e975ac4254c7d407
martin-san <developfreds@gmail.com>
2022-06-17 15:47:25 -0500

    creando una nuava clase hola mundo

---
+++ Proyecto_ejemplo1/src/com/utpl/ejemplo1/Saludos.java
@@ -0,0 +1,11 @@
+package com.utpl.ejemplo1;
+
+public class Saludos {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Saludando de nuevo....");
+
+	}
+
+}
---
+++ src/com/utpl/saludo/HolaMundo.java
@@ -0,0 +1,11 @@
+package com.utpl.saludo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("");
+
+	}
+
+}
